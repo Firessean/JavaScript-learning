d7d63f684f6190989693c78d6a1e0c53d81885d6
Firessean <themrsaus@gmail.com>
2022-11-02 20:52:27 +0200

    Start doing first steps for this project. Add click event for 'click button'. Add first scenario when we have empty field.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉Correct Number!';
 
@@ -7,3 +8,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 13;
 console.log(document.querySelector('.guess').value);
+ */
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+    }
+});
