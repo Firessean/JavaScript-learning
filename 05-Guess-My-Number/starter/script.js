44c4e02121d0451bceee432de595ce728506f612
Firessean <themrsaus@gmail.com>
2022-11-03 17:59:34 +0200

    Coding Challenge 1 was completed. Added functionality for again button.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,9 +10,8 @@ document.querySelector('.guess').value = 13;
 console.log(document.querySelector('.guess').value);
  */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = String(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        document.querySelector('.number').textContent = String(secretNumber);
 
         // When guess is too high
     } else if (guess > secretNumber) {
@@ -52,4 +53,18 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = String('0');
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+   score = 20;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+   document.querySelector('.guess').value = '';
+   document.querySelector('.score').textContent = String(score);
+   document.querySelector('.number').textContent = String(secretNumber);
+   document.querySelector('.message').textContent = 'Start guessing...';
+   document.querySelector('.number').textContent = '?';
+
+   document.querySelector('body').style.backgroundColor = '#222';
+   document.querySelector('.number').style.width = '15rem';
 });
