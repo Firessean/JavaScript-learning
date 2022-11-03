d3c556caf01be80a8299a92d2e82fb63b96a633a
Firessean <themrsaus@gmail.com>
2022-11-03 18:31:50 +0200

    Implemented highscore functionality.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -24,12 +25,15 @@ document.querySelector('.check').addEventListener('click', function () {
         // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = String(secretNumber);
 
         document.querySelector('body').style.backgroundColor = '#60b347';
-
         document.querySelector('.number').style.width = '30rem';
 
-        document.querySelector('.number').textContent = String(secretNumber);
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // When guess is too high
     } else if (guess > secretNumber) {
