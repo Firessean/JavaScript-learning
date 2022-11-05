194a05cfbeaa858037b9966ffb6b8f7e65be3577
Firessean <themrsaus@gmail.com>
2022-11-05 18:51:46 +0200

    Added resetting the game functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,14 +13,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = String(0);
-score1El.textContent = String(0);
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = String(0);
+    score1El.textContent = String(0);
+    current0El.textContent = String(0);
+    current1El.textContent = String(0);
+
+    diceEl.classList.add('hidden');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+};
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = String(0);
@@ -62,7 +75,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = String(scores[activePlayer]);
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             // Finish the game
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -74,3 +87,5 @@ btnHold.addEventListener('click', function () {
         }
     }
 });
+
+btnNew.addEventListener('click', init);
