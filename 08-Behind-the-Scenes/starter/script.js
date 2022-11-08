6577283ea5d3cc50b044c53c5dd424bb81c7754a
Firessean <themrsaus@gmail.com>
2022-11-08 19:46:39 +0200

    Learned how hoisting work and what is temporal dead zone

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
 
@@ -38,3 +38,48 @@ calcAge(1991);
 
 // console.log(age);
 // printAge();
+*/
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShopCart();
+
+var numProducts = 10;
+
+function deleteShopCart() {
+    console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
