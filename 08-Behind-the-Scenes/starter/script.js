14bcbeffb217b4d80ed0b5071bda5d8992bd668b
Firessean <themrsaus@gmail.com>
2022-11-09 18:58:52 +0200

    Learned difference between regular and arrow functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -84,7 +84,7 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
  */
-
+/*
 // this keyword
 
 console.log(this);
@@ -92,13 +92,13 @@ console.log(this);
 const calcAge = function (birthYear) {
     console.log(2037 - birthYear);
     console.log(this);
-}
+};
 calcAge(1991);
 
 const calcAgeArrow = birthYear => {
     console.log(2037 - birthYear);
     console.log(this);
-}
+};
 calcAgeArrow(1980);
 
 const jonas = {
@@ -106,16 +106,67 @@ const jonas = {
     calcAge: function () {
         console.log(this);
         console.log(2037 - this.year);
-    }
+    };
 }
 jonas.calcAge();
 
 const matilda = {
     year: 2017,
-}
+};
 
 matilda.calcAge = jonas.calcAge;
 matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+ */
+
+// Regular functions vs Arrow functions
+// var firstName = 'Matilda';
+
+const jonas = {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+
+        // Solution 1
+        // const self = this; // self or that
+        // const isMillenial = function () {
+        //     // console.log(this);
+        //     console.log(self);
+        //     // console.log(this.year >= 1981 && this.year <= 1996)
+        //     console.log(self.year >= 1981 && self.year <= 1996)
+        // };
+
+        // Solution 2
+        const isMillenial = () => {
+            // console.log(this);
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996)
+        };
+
+        isMillenial();
+    },
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`)
+    }
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExp = function (a, b) {
+    console.log(arguments);
+    return a + b;
+}
+addExp(2, 5);
+addExp(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+addArrow(2, 5, 8);
