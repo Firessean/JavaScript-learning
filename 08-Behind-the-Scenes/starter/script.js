6ce05c421c29c0efac14bbeef9a4043f20e818a5
Firessean <themrsaus@gmail.com>
2022-11-09 19:51:31 +0200

    Learned difference between primitive types and reference types

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -120,7 +120,7 @@ matilda.calcAge();
 const f = jonas.calcAge;
 f();
  */
-
+/*
 // Regular functions vs Arrow functions
 // var firstName = 'Matilda';
 
@@ -170,3 +170,59 @@ var addArrow = (a, b) => {
     return a + b;
 };
 addArrow(2, 5, 8);
+ */
+/*
+// lecture Primitives vs. Objects (Primitive vs. Reference Types)
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Me:', me);
+console.log('Friend:', friend);
+ */
+
+// Practice Primitives vs. Objects (Primitive vs. Reference Types)
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
