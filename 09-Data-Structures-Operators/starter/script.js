413c54c56ffb8a5e864845cfe4d02726878e47f7
Firessean <themrsaus@gmail.com>
2022-11-18 22:53:42 +0200

    Learned about enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,23 @@ const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thi', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,40 +29,31 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
+    // ES6 enhanced object literals
+    openingHours,
 
-    order: function (starterIndex, mainIndex) {
+    order(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
-    orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
         console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be 
         delivered to ${address} at ${time}`);
     },
 
-    orderPasta: function (ing1, ing2, ing3) {
+    orderPasta(ing1, ing2, ing3) {
         console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
     },
 
-    orederPizza: function (mainIngredient, ...otherIngredients) {
+    orderPizza(mainIngredient, ...otherIngredients) {
         console.log(mainIngredient);
         console.log(otherIngredients);
     },
 };
 
+//
+
+/*
 // Looping Arrays_ The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -54,6 +62,7 @@ for (const [item] of menu) console.log(item);
 for (const [i, el] of menu.entries()) {
     console.log(`${i + 1}: ${el}`);
 }
+ */
 
 // console.log([...menu.entries()]);
 
@@ -198,11 +207,11 @@ console.log(7 && 'Jonas');
 console.log('Hello' && 23 && null && 'Jonas');
 
 // Practical example
-if (restaurant.orederPizza) {
-    restaurant.orederPizza('mushrooms', 'spinach');
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
 }
 
-restaurant.orederPizza && restaurant.orederPizza('mushrooms', 'spinach');
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
  */
 
 /*
@@ -238,8 +247,8 @@ add(8, 2, 5, 3, 2, 1, 4);
 const x = [23, 5, 7];
 add(...x);
 
-restaurant.orederPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orederPizza('mushrooms');
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
  */
 
 /*
