85057dc018a483e3230cff7a3824dfbf2c86b00e
Firessean <themrsaus@gmail.com>
2022-12-21 20:19:34 +0200

    Learned about callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -27,7 +27,7 @@ createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
  */
-
+/*
 // How Passing Arguments Works_ Value vs. Reference
 const flight = 'LH234';
 const jonas = {
@@ -60,3 +60,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+ */
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+const high5 = function () {
+  console.log('✋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
