cf94fce13f144463cda3d8d1d22068ed9f1dda27
Firessean <themrsaus@gmail.com>
2023-01-05 22:36:52 +0200

    Learned how map method works

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -92,7 +92,7 @@ displayMovements(account1.movements);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -205,7 +205,7 @@ old.
 
     GOOD LUCK
  */
-
+/*
 const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
@@ -223,3 +223,25 @@ console.log('---- Data 1 ----');
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log('---- Data 2 ----');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+ */
+
+// The map Method
+const euroToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDFor = [];
+for (const mov of movements) {
+  movementsUSDFor.push(mov * euroToUsd);
+}
+console.log(movementsUSDFor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
